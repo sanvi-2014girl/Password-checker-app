e27d9f4018f793d559e0e5db8ca1e1d35098a3b5
sanvi-2014girl <goswamilipi82@gmail.com>
2024-10-18 20:24:10 +0530

    password

---
+++ main.py
@@ -0,0 +1,15 @@
+import re
+def check_password_strength(password):
+    if len(password) < 8:
+        return "Weak"
+    if not re.search("[a-z]", password):
+        return "Weak"
+    if not re.search("[A-Z]",password):
+        return "Weak"
+    if not re.search("[0-9]",password):
+        return "Weak"
+    if not re.search("[@#$%^&*()?<>,.+=-_*/\|`~]",password):
+        return "Weak"
+    return "Strong"
+password = input("Enter a password: ")
+print(f"Password strength: {check_password_strength(password)}")
